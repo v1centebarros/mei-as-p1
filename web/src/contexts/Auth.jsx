ffad0d1d0ec9d3316b3cf2d04db802eb3a00104b
v1centebarros <vmabarros@ua.pt>
2024-03-03 17:10:57 +0000

    Moved login and register mutation to the auth hook

--- web/src/contexts/Auth.jsx
+++ web/src/contexts/Auth.jsx
@@ -1,40 +1,17 @@
 import {createContext, useContext, useEffect, useMemo} from 'react';
 import {useAuth} from '../hooks/useAuth';
-import {useMutation} from "@tanstack/react-query";
-import {doLogin, doRegister} from "../../api/auth.js";
 
 export const AuthContext = createContext({});
 
 export const AuthProvider = ({children}) => {
-    const {login, isLogged, token, setToken,logout} = useAuth();
+    const {login, isLogged, token, setToken, logout, loginMutation, registerMutation} = useAuth();
 
     useEffect(() => {
         const t = localStorage.getItem('token');
         if (t) {
-            setToken(()=>t)
+            setToken(() => t)
         }
     }, []);
-
-    const loginMutation = useMutation({
-        mutationFn: (userData) => doLogin(userData),
-        onSuccess: (data) => {
-            setToken(()=>data.token)
-            localStorage.setItem('token', data.token)
-        },
-        onError: (error) => {
-            console.log('error', error)
-        }
-    });
-
-    const registerMutation = useMutation({
-        mutationFn:(userData) => doRegister(userData),
-        onSuccess: (data) => {
-            console.log('data', data)
-        },
-        onError: (error) => {
-            console.log('error', error)
-        }
-    })
 
     const authCtx = useMemo(() => {
             return {
--- web/src/hooks/useAuth.js
+++ web/src/hooks/useAuth.js
@@ -1,7 +1,31 @@
 import { useState } from 'react';
+import {useMutation} from "@tanstack/react-query";
+import {doLogin, doRegister} from "../../api/auth.js";
 
 export const useAuth = () => {
     const [token, setToken] = useState(null);
+
+
+    const loginMutation = useMutation({
+        mutationFn: (userData) => doLogin(userData),
+        onSuccess: (data) => {
+            setToken(()=>data.token)
+            localStorage.setItem('token', data.token)
+        },
+        onError: (error) => {
+            console.log('error', error)
+        }
+    });
+
+    const registerMutation = useMutation({
+        mutationFn:(userData) => doRegister(userData),
+        onSuccess: (data) => {
+            console.log('data', data)
+        },
+        onError: (error) => {
+            console.log('error', error)
+        }
+    })
 
     const login = (token) => {
         setToken(token);
@@ -21,6 +45,8 @@ export const useAuth = () => {
         logout,
         isLogged,
         token,
-        setToken
+        setToken,
+        loginMutation,
+        registerMutation
     }
 }
